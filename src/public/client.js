a45748f9a9c20b0d99dc1d1347c95ab85b14b2a8
wtface <nkhpark@hotmail.com>
2020-09-20 19:30:17 +0800

    refactor: separate render from updateStore, updateStore only updates nothing else. render() updates specific DOM only when called with specific component. This is to update only affected DOM.

--- src/public/client.js
+++ src/public/client.js
@@ -4,77 +4,73 @@ let store = {
     date: ''
 }
 
-// add our markup to the page
-const root = document.getElementById('root')
+const head = document.getElementById('head')
+// const rover = document.getElementById('rover')
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
-    render(root, store)
 }
 
-const render = async (root, state) => {
-    root.innerHTML = App(state)
+const render = async (dom, component) => {
+    dom.innerHTML = App(component)
 }
 
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header><h1>Astronomy Picture of the Day</h1></header>
-        <main>
-            <section>                
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
+const App = (component) => {
+    return component(store)
 }
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
     const today = new Date().toLocaleDateString().split('/')
     store.date = `${today[2]}-${today[1]}-${today[0]}`
-
-    render(root, store)
+    render(head, ImageOfTheDay)
 })
 
 // ------------------------------------------------------  COMPONENTS
-const roverMenu = (rovers) => {
-    let elem = '';
-    for(const r of rovers){
-        elem += `<button class="">${r}</button>`
-    }
-    return elem
-}
+// const roverMenu = (rovers) => {
+//     let elem = '';
+//     for(const r of rovers){
+//         elem += `<button class="">${r}</button>`
+//     }
+//     return elem
+// }
 
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-    // If image does not already exist, or it is not from today -- request it again
-    !apod && getImageOfTheDay(store);
+// Example of a pure function
+const ImageOfTheDay = (state) => {
+    const {apod} = state
+    !apod && getImageOfTheDay(state);
     
     if (apod.image.code === 404 || apod.image.code === 400) {
         const now = Date.now();
         let yesterday = new Date(now - 24 * 3600 * 1000)
         yesterday = `${yesterday.getFullYear()}-${yesterday.getMonth()+1}-${yesterday.getDate()}`
-        getImageOfTheDay(store, yesterday);
+        getImageOfTheDay(state, yesterday);
     }
 
-    // check if the photo of the day is actually type video!
+    let apodSection = ''
     if (apod.media_type === "video") {
-        return (`
+        apodSection =
+        `
             <p>See today's featured video <a href="${apod.url}">here</a></p>
             <p>${apod.title}</p>
             <p>${apod.explanation}</p>
-        `)
+        `
     } else {
-        return (`
+        apodSection = 
+        `
             <h3>${apod.image.title}</h3>
             <img src="${apod.image.url}" height="350px" width="100%" />
-        `)
+        `
     }
+    return `
+        <section>
+            <h2>Astronomy Picture of the Day</h2>
+            ${apodSection}
+        </section>
+    `
 }
 
 // ------------------------------------------------------  API CALLS
@@ -85,5 +81,8 @@ const getImageOfTheDay = (state, yesterday) => {
 
     fetch(`http://localhost:3000/apod/${date}`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(apod => {
+            updateStore(store, { apod });
+            render(head, ImageOfTheDay)
+        })
 }
